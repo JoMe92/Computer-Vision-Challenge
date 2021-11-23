da218d291e17fe044a5bbff312ef34e3be12885a
JoMe92 <jonasmeier@posteo.de>
2021-11-23 19:44:46 +0100

    add logic to the measure button

--- Gui/mainwindow.py
+++ Gui/mainwindow.py
@@ -19,10 +19,10 @@ class MainWindow(QMainWindow):
         # Call the corresponding functions when interacting with the gui
         self.ui.pushButton.clicked.connect(self.loadImage)
         self.ui.pushButton_2.clicked.connect(self.savePhoto)
-        # self.ui.pushButton_3.clicked.connect()
+        self.ui.pushButton_3.clicked.connect(self.measure_dist)
 
 
-        # initalization of the default parameters 
+        # initalization of the default parameters
         self.filename = 'Img_'+str(time.strftime("%Y-%b-%d_at_%H.%M.%S %p"))+'.png' # Will hold the image address location
         self.tmp = None # Will hold the temporary image for display
 
@@ -33,30 +33,41 @@ class MainWindow(QMainWindow):
         """
         self.filename = QFileDialog.getOpenFileName(filter="Image (*.*)")[0]
         self.image = cv2.imread(self.filename)
-        self.image = core.plot_diameter(self.image, 500,500,50)
+        # self.image = core.plot_diameter(self.image, 500,500,50)
 
 
         self.setPhoto(self.image)
-                    
+
+    def measure_dist(self, image):
+        """
+
+
+        """
+        y1 = [100,300,600]
+        for y in y1:
+            x, w = core.get_diameter(self.image, y)
+            self.image = core.plot_diameter(self.image, x, y, w)
+        #self.image = core.plot_diameter(self.image, 500,500,50)
+        self.setPhoto(self.image)
 
     def setPhoto(self,image):
-        """ This function will take image input and resize it 
+        """ This function will take image input and resize it
             only for display purpose and convert it to QImage
             to set at the label.
         """
         self.tmp = image
         # image = imutils.resize(image,width=640)
-        frame =  image # np.dstack([image, image, image]) # only for B&W image to get (x,x,3) shape 
+        frame =  image # np.dstack([image, image, image]) # only for B&W image to get (x,x,3) shape
         image = QtGui.QImage(frame, frame.shape[1],frame.shape[0],frame.strides[0],QtGui.QImage.Format_RGB888)
         self.ui.label.setPixmap(QtGui.QPixmap.fromImage(image))
 
     def update(self):
-        """ This function will update the photo according to the 
+        """ This function will update the photo according to the
             current values of blur and brightness and set it to photo label.
         """
         img = self.image
         self.setPhoto(img)
-        # self.savePhoto()     
+        # self.savePhoto()
 
 
     def savePhoto(self):
